eb518c95c33a21d056e445df94021b1d6ebd4633
Chandramani Kumar <56932293+hackifme1@users.noreply.github.com>
2021-05-28 20:58:59 +0530

    Update and rename Find All Duplicates in an Array to Find All Duplicates in an Array.cpp

--- Array/Find All Duplicates in an Array.cpp
+++ Array/Find All Duplicates in an Array.cpp
@@ -2,7 +2,7 @@
 // Keep moving and must be simple bro!!!!!
 // Espérons le meilleur mais préparez-vous au pire 😎
 
-
+/* Problem Statement
 Given an integer array nums of length n where all the integers of nums are in the range [1, n] and each integer appears once or twice, return an array of all the integers that appears twice.
 
 You must write an algorithm that runs in O(n) time and uses only constant extra space.
@@ -29,6 +29,8 @@ n == nums.length
 1 <= n <= 105
 1 <= nums[i] <= n
 Each element in nums appears once or twice.
+
+*/
 
 Cpp code :
 
