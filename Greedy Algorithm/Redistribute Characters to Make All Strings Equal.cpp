0944a12bdefb5cddea939635be739b91b107650c
Chandramani Kumar <56932293+hackifme1@users.noreply.github.com>
2021-06-13 14:37:03 +0530

    Added Cpp code for Redistribute Characters to Make All Strings Equal

---
+++ Greedy Algorithm/Redistribute Characters to Make All Strings Equal.cpp
@@ -0,0 +1,66 @@
+// By Chandramani Kumar
+// Keep moving and must be simple bro!!!!!
+// Espérons le meilleur mais préparez-vous au pire 😎
+
+
+/* Problem Statement :
+
+You are given an array of strings words (0-indexed).
+
+In one operation, pick two distinct indices i and j, where words[i] is a non-empty string, and move any character from words[i] to any position in words[j].
+
+Return true if you can make every string in words equal using any number of operations, and false otherwise.
+
+
+Example 1:
+
+Input: words = ["abc","aabc","bc"]
+Output: true
+
+Explanation: Move the first 'a' in words[1] to the front of words[2],
+to make words[1] = "abc" and words[2] = "abc".
+All the strings are now equal to "abc", so return true.
+
+Example 2:
+
+Input: words = ["ab","a"]
+Output: false
+
+Explanation: It is impossible to make all the strings equal using the operation.
+ 
+
+Constraints:
+
+1 <= words.length <= 100
+1 <= words[i].length <= 100
+words[i] consists of lowercase English letters.
+
+*/
+
+
+Cpp code :
+
+class Solution {
+public:
+    bool makeEqual(vector<string>& words) {
+       int n = words.size();
+        
+        int f[26]={0};
+        
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<words[i].size();j++)
+            {
+                f[words[i][j]-'a']++;
+            }
+        }
+        
+        for(int i=0;i<26;i++)
+        {
+            if(f[i]>0 && f[i]%n!=0)
+                return false;
+        }
+        
+        return true;
+    }
+};
