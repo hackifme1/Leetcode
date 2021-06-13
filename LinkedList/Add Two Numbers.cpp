1344caa2577b46693fc3c6309d0acfc0dccc64d9
Chandramani Kumar <56932293+hackifme1@users.noreply.github.com>
2021-06-13 14:40:14 +0530

    Update and rename Add Two Numbers to Add Two Numbers.cpp

--- LinkedList/Add Two Numbers.cpp
+++ LinkedList/Add Two Numbers.cpp
@@ -2,6 +2,92 @@
 // Keep moving and must be simple bro!!!!!
 // Espérons le meilleur mais préparez-vous au pire 😎
 
+/* Problem Statement :
+
+
+*/
+
+Cpp code :
+
+// Linked lists only
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode* temp1 = l1;
+        ListNode* temp2 = l2;
+        ListNode* l3 = new ListNode();
+        l3->val = (temp1->val + temp2->val)%10;
+        int carr = (temp1->val + temp2->val)/10;
+        l3->next = NULL;
+        temp1=temp1->next;
+        temp2=temp2->next;
+       
+        ListNode* temp3 = l3;
+        while(temp1 != NULL && temp2 != NULL)
+        {
+            int s = (temp1->val + temp2->val + carr)%10;
+            carr = (temp1->val + temp2->val + carr)/10;
+            
+            ListNode* t = new ListNode();
+            t->val = s;
+            t->next = NULL;
+            temp3 ->next = t;
+            temp3 = temp3->next;
+             temp1=temp1->next;
+             temp2=temp2->next;
+        }
+        
+        while(temp1!=NULL)
+        {
+             int s = (temp1->val + carr)%10;
+            carr = (temp1->val + carr)/10;
+            ListNode* t = new ListNode();
+            t->val = s;
+            t->next = NULL;
+            temp3 ->next = t;
+            temp3 = temp3->next;
+            temp1=temp1->next;
+        }
+        
+        while(temp2!=NULL)
+        {
+            int s = (temp2->val + carr)%10;
+            carr = (temp2->val + carr)/10;
+            ListNode* t = new ListNode();
+            t->val = s;
+            t->next = NULL;
+            temp3->next = t;
+            temp3 = temp3->next;
+            temp2 = temp2->next;
+        }
+        
+        if(carr>0)
+        {
+            ListNode* t = new ListNode();
+            t->val = carr;
+            t->next = NULL;
+            temp3->next = t;
+            temp3 = temp3->next;
+        }
+        
+        return l3;
+    }
+};
+
+
+// Using vectors (not recommended)
+
 /**
  * Definition for singly-linked list.
  * struct ListNode {
