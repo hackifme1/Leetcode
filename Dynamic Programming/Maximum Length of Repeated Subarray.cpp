ae2b1d2d4cb3d44df01dab5c1066c5aab25b6e94
Chandramani Kumar <56932293+hackifme1@users.noreply.github.com>
2021-07-08 13:36:37 +0530

    Added Cpp code for Maximum Length of Repeated Subarray

---
+++ Dynamic Programming/Maximum Length of Repeated Subarray.cpp
@@ -0,0 +1,65 @@
+// By Chandramani Kumar
+// Keep moving and must be simple bro!!!!!
+// Espérons le meilleur mais préparez-vous au pire 😎
+
+
+/* Problem Statement :
+
+Given two integer arrays nums1 and nums2, return the maximum length of a subarray that appears in both arrays.
+
+
+Example 1:
+
+Input: nums1 = [1,2,3,2,1], nums2 = [3,2,1,4,7]
+Output: 3
+
+Explanation: The repeated subarray with maximum length is [3,2,1].
+
+Example 2:
+
+Input: nums1 = [0,0,0,0,0], nums2 = [0,0,0,0,0]
+Output: 5
+ 
+
+Constraints:
+
+1 <= nums1.length, nums2.length <= 1000
+0 <= nums1[i], nums2[i] <= 100
+
+*/
+
+
+Cpp code :
+
+class Solution {
+public:
+    int findLength(vector<int>& nums1, vector<int>& nums2) {
+        int n = nums1.size();
+        int m = nums2.size();
+        
+        vector<vector<int>> dp(n+1,vector<int>(m+1,0));
+        
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=m;j++)
+            {
+                if(nums1[i-1] == nums2[j-1])
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                else
+                    dp[i][j] = 0;
+            }
+        }
+        
+        int maxm = INT_MIN;
+        
+         for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=m;j++)
+            {
+                maxm = max(maxm,dp[i][j]);
+            }
+         }
+        
+        return maxm;
+    }
+};
