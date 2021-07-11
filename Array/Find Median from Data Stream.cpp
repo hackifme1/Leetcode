ebfde9386cbf6c9065eac7cc53a768458d0fc32b
Chandramani Kumar <56932293+hackifme1@users.noreply.github.com>
2021-07-11 14:55:32 +0530

    Added Cpp code for Find Median from Data Stream

---
+++ Array/Find Median from Data Stream.cpp
@@ -0,0 +1,93 @@
+// By Chandramani Kumar
+// Keep moving and must be simple bro!!!!!
+// Espérons le meilleur mais préparez-vous au pire 😎
+
+
+/* Problem Statement :
+
+The median is the middle value in an ordered integer list. If the size of the list is even, there is no middle value and the median is the mean of the two middle values.
+
+For example, for arr = [2,3,4], the median is 3.
+For example, for arr = [2,3], the median is (2 + 3) / 2 = 2.5.
+Implement the MedianFinder class:
+
+MedianFinder() initializes the MedianFinder object.
+void addNum(int num) adds the integer num from the data stream to the data structure.
+double findMedian() returns the median of all elements so far. Answers within 10-5 of the actual answer will be accepted.
+ 
+
+Example 1:
+
+Input
+["MedianFinder", "addNum", "addNum", "findMedian", "addNum", "findMedian"]
+[[], [1], [2], [], [3], []]
+
+Output
+[null, null, null, 1.5, null, 2.0]
+
+Explanation
+MedianFinder medianFinder = new MedianFinder();
+medianFinder.addNum(1);    // arr = [1]
+medianFinder.addNum(2);    // arr = [1, 2]
+medianFinder.findMedian(); // return 1.5 (i.e., (1 + 2) / 2)
+medianFinder.addNum(3);    // arr[1, 2, 3]
+medianFinder.findMedian(); // return 2.0
+ 
+
+Constraints:
+
+-105 <= num <= 105
+There will be at least one element in the data structure before calling findMedian.
+At most 5 * 104 calls will be made to addNum and findMedian.
+ 
+
+Follow up:
+
+If all integer numbers from the stream are in the range [0, 100], how would you optimize your solution?
+If 99% of all integer numbers from the stream are in the range [0, 100], how would you optimize your solution?
+
+*/
+
+
+Cpp code :
+
+class MedianFinder {
+public:
+    /** initialize your data structure here. */
+    multiset<int> st;
+    MedianFinder() {
+        
+    }
+    
+    void addNum(int num) {
+        st.insert(num);
+    }
+    
+    double findMedian() {
+        int n = st.size();
+        auto it=st.begin();
+        
+        for(int i=0;i<n/2;i++) 
+            it++;
+    
+         if(n%2)
+           return *it;         
+         else
+         {
+            double d1= *it;
+            it--;
+             
+            double d2=*it;
+            double res = (d1+d2)/2.0;
+             
+            return res;
+        }
+    }
+};
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * MedianFinder* obj = new MedianFinder();
+ * obj->addNum(num);
+ * double param_2 = obj->findMedian();
+ */
