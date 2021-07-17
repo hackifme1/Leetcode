7a3b1f17807693a747327896217d116264601d30
Chandramani Kumar <56932293+hackifme1@users.noreply.github.com>
2021-07-17 20:40:56 +0530

    Added Cpp code for Construct String from Binary Tree

---
+++ Binary Tree/Construct String from Binary Tree.cpp
@@ -0,0 +1,70 @@
+// By Chandramani Kumar
+// Keep moving and must be simple bro!!!!!
+// Espérons le meilleur mais préparez-vous au pire 😎
+
+
+/* Problem Statement :
+
+Given the root of a binary tree, construct a string consists of parenthesis and integers from a binary tree with the preorder traversing way, and return it.
+
+Omit all the empty parenthesis pairs that do not affect the one-to-one mapping relationship between the string and the original binary tree.
+
+ 
+
+Example 1:
+
+Input: root = [1,2,3,4]
+Output: "1(2(4))(3)"
+
+Explanation: Originallay it needs to be "1(2(4)())(3()())", but you need to omit all the unnecessary empty parenthesis pairs. And it will be "1(2(4))(3)"
+
+Example 2:
+
+Input: root = [1,2,3,null,4]
+Output: "1(2()(4))(3)"
+
+Explanation: Almost the same as the first example, except we cannot omit the first parenthesis pair to break the one-to-one mapping relationship between the input and the output.
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [1, 104].
+-1000 <= Node.val <= 1000
+
+*/
+
+
+Cpp code :
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:    
+    string tree2str(TreeNode* root) {
+        
+        if (root == NULL) 
+            return "";
+        
+        string s = to_string(root->val);
+        
+        if (root->left) 
+            s += '(' + tree2str(root->left) + ')';
+        else if (root->right)
+            s += "()";
+        
+        if (root->right) 
+            s += '(' + tree2str(root->right) + ')';
+        
+        return s;
+    }
+};
