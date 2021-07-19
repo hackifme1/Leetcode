00b0edb376cfe63eb4cf784eb19e950bbcfdbd43
Chandramani Kumar <56932293+hackifme1@users.noreply.github.com>
2021-07-19 14:43:12 +0530

    Added Cpp code for Reverse Nodes in k-Group

---
+++ LinkedList/Reverse Nodes in k-Group.cpp
@@ -0,0 +1,91 @@
+// By Chandramani Kumar
+// Keep moving and must be simple bro!!!!!
+// Espérons le meilleur mais préparez-vous au pire 😎
+
+
+/* Problem Statement :
+
+Given a linked list, reverse the nodes of a linked list k at a time and return its modified list.
+
+k is a positive integer and is less than or equal to the length of the linked list. If the number of nodes is not a multiple of k then left-out nodes, in the end, should remain as it is.
+
+You may not alter the values in the list's nodes, only nodes themselves may be changed.
+
+ 
+
+Example 1:
+
+Input: head = [1,2,3,4,5], k = 2
+Output: [2,1,4,3,5]
+
+Example 2:
+
+Input: head = [1,2,3,4,5], k = 3
+Output: [3,2,1,4,5]
+
+Example 3:
+
+Input: head = [1,2,3,4,5], k = 1
+Output: [1,2,3,4,5]
+
+Example 4:
+
+Input: head = [1], k = 1
+Output: [1]
+ 
+
+Constraints:
+
+The number of nodes in the list is in the range sz.
+1 <= sz <= 5000
+0 <= Node.val <= 1000
+1 <= k <= sz
+ 
+
+Follow-up: Can you solve the problem in O(1) extra memory space?
+
+*/
+
+
+Cpp code :
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        ListNode* crr = head;
+        int cnt = 0;
+        
+        while (crr != NULL && cnt != k) 
+        { 
+            crr = crr->next;
+            cnt++;
+        }
+        
+        if (cnt == k) 
+        { 
+            crr = reverseKGroup(crr, k); 
+            
+            while (cnt--) 
+            { 
+                ListNode* tmp = head->next; 
+                head->next = crr; 
+                crr = head; 
+                head = tmp; 
+            }
+            
+            head = crr;
+        }
+        
+        return head;
+    }
+};
